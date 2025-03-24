8aafe71f43ff1cf2f9146706074cef6a90595e41
itamab <itamab@gmail.com>
2025-03-24 10:19:11 +0200

    simplified gui agent example

--- src/agent/supervisor/types.ts
+++
@@ -1,9 +0,0 @@
-import { Annotation } from "@langchain/langgraph";
-import { GenerativeUIAnnotation } from "../types";
-
-export const SupervisorAnnotation = Annotation.Root({
-  ...GenerativeUIAnnotation.spec,
-});
-
-export type SupervisorState = typeof SupervisorAnnotation.State;
-export type SupervisorUpdate = typeof SupervisorAnnotation.Update;
